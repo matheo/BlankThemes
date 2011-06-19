790b98455b01f52217bc5c5ebfb672228bef8cb9
Mateo Tibaquirá Palacios <mateo@zikula.org>
2011-06-18 22:42:10 -0500

    [arcsin] SimpleOrganization added. Latest BlankTheme folder renamed.

---
+++ arcsin/SimpleOrganization/templates/blocks/menublock.tpl
@@ -0,0 +1,10 @@
+
+<!-- Menu Block -->
+{if $title neq ''}{* Display title if there is one *}
+<h4 class="z-block-title">
+    <span>{$title}</span>
+</h4>
+{/if}
+<div class="z-block-content bt-menublock">
+    {$content}
+</div>
---
+++ arcsin/SimpleOrganization/version.php
@@ -0,0 +1,32 @@
+<?php 
+/**
+ * Zikula Application Framework
+ *
+ * @copyright (c) 2007-2011 Mateo Tibaquirá
+ * @link      http://www.blanktheme.org
+ * @license   GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @abstract  Blank theme to develop new themes with ease
+ */
+
+
+$dom = ZLanguage::getThemeDomain('BlankTheme');
+
+$themeversion['name']           = 'BlankTheme';
+$themeversion['displayname']    = __('BlankTheme', $dom);
+$themeversion['description']    = __('Theme development framework for Zikula', $dom);
+$themeversion['version']        = '1.3';
+
+$themeversion['author']         = 'BlankTheme Team';
+$themeversion['contact']        = 'http://www.blanktheme.org';
+$themeversion['official']       = '0';
+$themeversion['admin']          = 1;
+$themeversion['user']           = 1;
+$themeversion['system']         = 0;
+$themeversion['changelog']      = 'docs/changelog.txt';
+$themeversion['credits']        = 'docs/credits.txt';
+$themeversion['help']           = 'docs/help.txt';
+$themeversion['license']        = 'docs/license.txt';
+$themeversion['xhtml']          = true;
+
+$themeversion['extra']          = array('BlankTheme' => '1.3',
+                                        'YAML'       => '3.3');
